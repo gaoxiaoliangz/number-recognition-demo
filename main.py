2df9d7863b50317bee104dabc405009caa41d5e0
liang <gaoxiaoliangz@gmail.com>
2017-01-23 00:47:03 +0800

    read pixels

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 """server"""
-from flask import Flask, url_for, render_template, request, jsonify
-import os, base64
+import base64
+import io
+import os
+
+import numpy
+from flask import Flask, jsonify, render_template, request, url_for
+
+from PIL import Image, ImageFilter
 
 app = Flask(__name__)
 
-strg = "thinking about "
-
+# strg = "thinking about "
 # @app.route("/<string:name>")
 # def move(name):
 #   return strg + name
@@ -16,19 +21,46 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_img():
-  img = request.form['img'][22:]
+  # 1
+  img_str = request.form['img'][22:]
+  img_data = base64.b64decode(img_str)
+  print(type(img_data))
+  # print(img_data.read())
+  img_obj = Image.open(io.BytesIO(img_data))
+  # print(type(img_obj))
 
-  # print(type(img))
-  
-  imgData = base64.b64decode(img)
-  leniyimg = open('imgout.png','wb')
-  leniyimg.write(imgData)
-  leniyimg.close()
+  img_pix = img_obj.load()
+  print(img_obj.size)
+  print(img_pix[0, 1])
+  print(type(img_pix))
 
-  # print(imgData)
+  # 2
+  # img_data = Image.open('pic2.png')
+  # print(img_data.size)
+
+  # 3 ok
+  # img_data = open('dw.jpg', 'rb')
+  # # print(img_data.read())
+  # # print(img_data.seek())
+  # # print(img_data.tell())
+  # img_obj = Image.open(img_data)
+  # img_pix = img_obj.load()
+  # print(img_obj.size)
+  # print(img_pix[0, 1])  
+
+  #Display image
+  # im.show()
+  # im_sharp = im.filter( ImageFilter.SHARPEN )
+  # r,g,b = im_sharp.split()
+  # print(r)
+
+
+  # write image
+  # leniyimg = open('imgout.png','wb')
+  # leniyimg.write(imgData)
+  # leniyimg.close()
 
   return jsonify(ok=1)
 
 if __name__ == '__main__':
-  # url_for('static', filename='style.css')
   app.run()
