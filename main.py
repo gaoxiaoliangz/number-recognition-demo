8227d63c63b861c64f0b19fb74cb479251b5eb12
liang <gaoxiaoliangz@gmail.com>
2017-01-23 09:45:15 +0800

    image read api ready

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 import base64
 import io
 import os
-
-import numpy
+import numpy as np
+# import numpy
 from flask import Flask, jsonify, render_template, request, url_for
 
 from PIL import Image, ImageFilter
@@ -15,6 +15,28 @@ app = Flask(__name__)
 # def move(name):
 #   return strg + name
 
+
+
+def print_image(raw_image_data):
+  """print image with '+' & '-'"""
+  one_sample_image = np.array(raw_image_data).reshape([28, 28])
+  for index_a in range(len(one_sample_image)):
+    for index_b in range(len(one_sample_image[index_a])):
+      if index_b == 27:
+        print('')
+
+      point = one_sample_image[index_a][index_b]
+
+      if point > 0.3:
+        print("+", end="")
+      elif point > 0 and point <= 0.3:
+        print("-", end="")
+      else:
+        print(" ", end="")
+
+
+
+
 @app.route('/')
 def index():
   return render_template('index.html', title='recog number')
@@ -24,15 +46,28 @@ def upload_img():
   # 1
   img_str = request.form['img'][22:]
   img_data = base64.b64decode(img_str)
-  print(type(img_data))
-  # print(img_data.read())
   img_obj = Image.open(io.BytesIO(img_data))
+  img_obj = img_obj.resize((28, 28))
   # print(type(img_obj))
 
   img_pix = img_obj.load()
-  print(img_obj.size)
-  print(img_pix[0, 1])
-  print(type(img_pix))
+
+  img_arr = []
+
+  for index_b in range(28):
+    for index_a in range(28):
+      pix_l = img_pix[index_a, index_b][3]/255
+      img_arr.append(pix_l)
+
+
+  print_image(img_arr)
+
+
+
+
+  # print(img_obj.size)
+  # print(img_pix[0, 1])
+  # print(type(img_pix))
 
   # 2
   # img_data = Image.open('pic2.png')
