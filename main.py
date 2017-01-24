211f4063e520314e4297faa1e3f08fc13e3156c2
liang <gaoxiaoliangz@gmail.com>
2017-01-24 19:06:24 +0800

    wtf

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 """server"""
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 import base64
 import io
 import os
+
 import numpy as np
 import tensorflow as tf
-# import numpy
+
 from flask import Flask, jsonify, render_template, request, url_for
+from tensorflow.examples.tutorials.mnist import input_data
 
 from PIL import Image, ImageFilter
 
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 
 def print_image(raw_image_data):
@@ -61,30 +62,36 @@ def train():
     sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
   print('end training')
+  # print(sess.run(W))
 
 
 def test_image(raw_image):
   """test images"""
 
-  # raw_image_32 = [float(raw_image_p) for raw_image_p in raw_image]
-  raw_image_32 = [np.float(raw_image_p) for raw_image_p in raw_image]
-  np_img = np.array(raw_image_32)
-  print(np.shape(np_img))
-  # print_image(np_img)
-  # print(np_img)
-  # print(len(np_img))
-  # print(type(np_img))
-  print(type(np_img[0]))
+  print(sess.run(W))
 
-  t_image = tf.pack([np_img], name='test_image')
-  result = tf.nn.softmax(tf.matmul(t_image, W) + b)
-  final_result = tf.argmax(result, 1)
-  py_res = sess.run(final_result)[0]
 
-  # print("number is: ", end="")
-  # print()
-  # print("---")
-  return py_res
+  # # raw_image_32 = [float(raw_image_p) for raw_image_p in raw_image]
+  # raw_image_32 = [np.float(raw_image_p) for raw_image_p in raw_image]
+  # np_img = np.array(raw_image_32)
+  # print(np.shape(np_img))
+  # # print_image(np_img)
+  # # print(np_img)
+  # # print(len(np_img))
+  # # print(type(np_img))
+  
+
+  # np_img = mnist.test.images[876]
+
+  # # print(type(np_img[0]))
+
+  # t_image = tf.pack([np_img], name='test_image')
+  # result = tf.nn.softmax(tf.matmul(t_image, W) + b)
+  # final_result = tf.argmax(result, 1)
+  # py_res = sess.run(final_result)[0]
+
+  # return py_res
+  return 1
 
 ## tf end
 
@@ -121,7 +128,8 @@ def upload_img():
 
 
   num = test_image(img_arr)
-
+  num = 'wtf'
+  
 
 
 
